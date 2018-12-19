c451d1fb8572b9d3c12482b14763f7e2620fd413
DeepayanMallick <deepayan1@live.com>
2018-12-20 00:02:43 +0600

    first commit

---
+++ website/urls.py
@@ -0,0 +1,14 @@
+from django.urls import re_path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^music/', include('music.urls')),
+    re_path(r'^', include('music.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
